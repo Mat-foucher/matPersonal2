3667280b662390bd4a0a63d020e2abfce8d7c01e
Mathieu Foucher <mafo8363@colorado.edu>
2023-03-29 10:55:21 -0600

    first commit

---
+++ form.php
@@ -0,0 +1,1 @@
+<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { $name = $_POST["name"]; $email = $_POST["email"]; $message = $_POST["message"]; $to = "mathieubfoucher@gmail.com"; $subject = "New message from $name"; $body = "Name: $name\nEmail: $email\nMessage:\n$message"; mail($to, $subject, $body); header("Location: thank-you.html"); exit; } ?> 
